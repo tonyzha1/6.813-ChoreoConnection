da27509ca17e488a26acac8c90986339194d9d29
tonyzha1 <tonyzha1@mit.edu>
2014-04-16 00:15:19 -0400

    Comment.js fix patch
    Added jsapi includes to choreographer and dancer htmls.
    
    Fixed a lot of bugs in comment.js and added some documentation.

--- js/comment.js
+++ js/comment.js
@@ -1,4 +1,9 @@
-//Comment.js
+// Comment.js
+// Insert comments into the array called comments (defined below).
+// Create comments using "new TimedComment".
+// This supports deleting comments, but the behavior relies on 
+// interactions using the "undefined" value. 
+
 function TimedComment(text, author, start, end){
 	this.text = text;
 	this.author= author;
@@ -7,24 +12,39 @@ function TimedComment(text, author, start, end){
 	this.isTimed = true;
 }
 
+// use createCommentDiv if you need to display the comment somewhere other than where displayComment puts it.
+function createCommentDiv(comment, commentID) {
+	var parentDiv = jQuery("<div>", {
+		id:"TimedComment_"+commentID });
+	var authorDiv = jQuery("<div>", { 
+		class:"authorDiv",
+		text:comment.author });
+	var commentDiv = jQuery("<div>", {
+		class:"commentDiv",
+		text:comment.text });
+	parentDiv[0].appendChild(commentDiv[0]);
+	parentDiv[0].appendChild(authorDiv[0]);
+	return parentDiv;
+}
+
 function displayComment(comment, commentID, parentSelector) { 
-	if($("#TimedComment_"+commentID)) {
-		return; 
+	if($("#TimedComment_"+commentID).length) {
+		return;
 	}
-	jQuery("<div/>", {
-		id:"TimedComment_"+commentID,
-		text:comment.Text, 
-	}).appendTo(parentSelector);
+
+	var result = createCommentDiv(comment, commentID);
+	result.appendTo(parentSelector);
+	
 }
 function removeComment(commentID, parentSelector) {
-	$(parentSelector).remove("#TimedComment_"+commentID);
+	$("#TimedComment_"+commentID).remove();
 }
 
 
 // Code starting here is lifted and adapted from Google Youtube API sandbox.
 google.load("swfobject", "2.1");
-comments = [ new Comment("This is a comment", "I am the author", 25, 30), 
-			new Comment("WTF is this", "ARGH", 27, 32)];
+comments = [ new TimedComment("This is a comment", "I am the author", 25, 30), 
+			new TimedComment("WTF is this", "ARGH", 27, 32)];
 
 function updatePlayerInfo() {
 	var videoCommentSelector = "#comments";
@@ -40,7 +60,7 @@ function updatePlayerInfo() {
 }
 
 
-function onYoutubePlayerReady(playerId) {
+function onYouTubePlayerReady(playerId) {
 	ytplayer = document.getElementById("ytPlayer");
 	setInterval(updatePlayerInfo, 250);
 	updatePlayerInfo();
@@ -60,5 +80,7 @@ function loadPlayer(){
 		"videoDiv", // name of containing element.
 		"480", "295", "9", null, null, params, atts);
 }
-
-google.setOnLoadCallback(loadPlayer);
+function _run() { 
+	loadPlayer();
+}
+google.setOnLoadCallback(_run);
